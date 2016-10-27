0f955c705d54a758cdffe1a5c2902b8d0c65bf5c
ahmohamed <ahmohamed@user.noreply.github.com>
2016-10-28 01:49:56 +0200

    rebuild js, css

--- typeahead-multiselect.js
+++ typeahead-multiselect.js
@@ -1,9 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
-},{}],2:[function(require,module,exports){
 (function (global){
 var $ = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
-var typeahead = require('typeahead.js');
 
 var addSelected = function($el, $selections, sel, display, template){
   $selections.find('.empty-selection').hide();
@@ -96,4 +93,4 @@ $.fn.typeaheadmulti = function(options, dataset){
 
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"typeahead.js":1}]},{},[2]);
+},{}]},{},[1]);
